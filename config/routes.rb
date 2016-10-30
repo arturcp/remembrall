1ccc7316b9c9aaa25cf8c4d83a36f9d793268e2f
Artur <artur.prado@gmail.com>
2016-10-29 23:05:02 -0200

    Revive message route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resource :message, only: :create
+
   post 'articles/search', to: 'articles#search'
   get 'articles/:search_type/:query', to: 'articles#index', as: :articles
 
