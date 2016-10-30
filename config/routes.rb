365810813d5b1d20472209cecfd262add611ee55
Artur <artur.prado@gmail.com>
2016-10-29 22:45:31 -0200

    Improve routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resource :message, only: :create
-  resources :articles, only: :index
-
   post 'articles/search', to: 'articles#search'
-  get 'articles/search', to: redirect('/articles')
+  get 'articles/:search_type/:query', to: 'articles#index', as: :articles
 
   root 'articles#index'
 end
