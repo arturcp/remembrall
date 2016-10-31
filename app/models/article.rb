06dcb2ac1120197d13423490252ed6a356be4727
Artur <artur.prado@gmail.com>
2016-10-31 12:40:50 -0200

    Insert custom image when none is provided

--- app/models/article.rb
+++ app/models/article.rb
@@ -17,8 +17,13 @@ class Article < ActiveRecord::Base
         article.url = url
         article.title = page_content.title
         article.description = page_content.description
-        article.image_url = page_content.images.first.src.to_s
         article.favicon = page_content.favicon
+
+        if page_content.images.first
+          article.image_url = page_content.images.first.src.to_s
+        else
+          article.image_url = 'http://media1.santabanta.com/full1/Creative/Abstract/abstract-749a.jpg'
+        end
       end.save!
     end
   end
--- app/models/message.rb
+++ app/models/message.rb
@@ -4,7 +4,10 @@ class Message
   URL_REGEX = /<(https?:\/\/(?:www.|(?!www))[^\s.]+.[^\s]{2,}|www.[^\s]+.[^\s]{2,})>/
   BLACK_LIST = [
     'hangouts.google.com',
-    'youse-remembrall.herokuapp.com'
+    'youse-remembrall.herokuapp.com',
+    'github.com',
+    'cxdigital.atlassian.net',
+    'docs.google.com'
   ]
 
   def initialize(text = '')
