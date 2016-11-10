e4c2a277dcb8a7c71448492f381015f950650d6f
Artur <artur.prado@gmail.com>
2016-11-10 11:00:29 -0200

    Simplify url regex to allow query string

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class URL
-  REGEX = /<(https?:\/\/(?:www.|(?!www))[^\s.]+.[^\s]{2,}|www.[^\s]+.[^\s]{2,})>/
+  REGEX = /<([^>]+)>/
   BLACK_LIST = [
     'hangouts.google.com',
     'youse-remembrall.herokuapp.com',
