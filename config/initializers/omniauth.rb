459650d7c17e69ff108bc4b90ab1709bdb4004c4
Artur <artur.prado@gmail.com>
2017-01-27 11:17:10 -0200

    Request user email

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :slack, ENV.fetch('SLACK_KEY'), ENV.fetch('SLACK_SECRET'), scope: 'identity.basic', team: ENV.fetch('TEAM_ID'), name: :sign_in_with_slack
-  provider :slack, ENV.fetch('SLACK_KEY'), ENV.fetch('SLACK_SECRET'), scope: 'team:read,users:read,identify'
+  provider :slack, ENV.fetch('SLACK_KEY'), ENV.fetch('SLACK_SECRET'), scope: 'team:read,users:read,identify,users:read.email'
 end
