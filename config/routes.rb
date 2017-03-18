588d6666c68a9e15483259799c9de920f91ab47c
Artur <artur.prado@gmail.com>
2017-03-18 14:03:24 -0300

    Add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resource :message, only: :create
+  get 'team/connect', to: 'teams#connect'
   resources :favorites, only: [:create, :index]
   resources :tags, only: :index
 
-  post 'articles/search', to: 'articles#search'
-  get 'articles/search/:query', to: 'articles#index', as: :articles
+  scope ':collection' do
+    get 'articles/search(/:query)', to: 'articles#index', as: :articles
+    post 'articles/search', to: 'articles#search'
+  end
 
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 
-  root 'articles#index'
+  root 'home#index'
 end
