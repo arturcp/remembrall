a90d23c9ebf8a382fd8b7cde78ae01f5767b40c9
Artur <artur.prado@gmail.com>
2017-03-18 14:02:41 -0300

    Crete teams controller

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class TeamsController < ApplicationController
+  def connect
+    collection = create_team(params['code'])
+
+    if collection
+      redirect_to articles_search_path(collection)
+    else
+      render text: 'Não foi possível conectar com o Remembrall'
+    end
+  end
+
+  private
+
+  def create_team(code)
+    return unless code.present?
+
+    response = SlackAPI.request_token(code)
+    Collection.create_from_oauth_response(response)
+  end
+end
