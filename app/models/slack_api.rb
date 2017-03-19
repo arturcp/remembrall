0a651183ea153a8fe7fe17a6a59028a6fcc48e63
Artur <artur.prado@gmail.com>
2017-03-18 22:59:48 -0300

    Explain methods and symbolize keys

--- app/models/slack_api.rb
+++ app/models/slack_api.rb
@@ -4,15 +4,47 @@ require 'net/http'
 require 'uri'
 
 class SlackAPI
+  OAUTH_ACCESS_URL = 'https://slack.com/api/oauth.access'
+  USERS_INFO_URL = 'https://slack.com/api/users.info'
+
+  # Public: Request an oauth token to allow future requests using oAuth2.
+  #
+  # code - it is provided by slack api, automatically included in the return url
+  #        configured in the slack app.
+  #
+  # An example of a valid response of the method can be found at `docs/oauth_access.json`
+  #
+  # It will post to https://slack.com/api/oauth.access
+  #
+  # Returns a JSON.
   def self.request_token(code)
-    uri = URI.parse('https://slack.com/api/oauth.access')
     params = {
       code: code,
       client_id: ENV.fetch('SLACK_CLIENT_ID'),
       client_secret: ENV.fetch('SLACK_CLIENT_SECRET')
     }
+
+    post(OAUTH_ACCESS_URL, params)
+  end
+
+  # Public: fetches info about a user.
+  #
+  # An example of a valid response of the method can be found at `docs/user_info.json`
+  #
+  # The token must have the users:read permission.
+  # It will post to https://slack.com/api/users.info
+  #
+  # Returns a JSON.
+  def self.user_info(token, user_id)
+    post(USERS_INFO_URL, { token: token, user: user_id })
+  end
+
+  def self.post(url, params)
+    uri = URI.parse(url)
     response = Net::HTTP.post_form(uri, params)
 
-    JSON.parse(response.body, symbolize_names: true)
+    JSON.parse(response.body).deep_symbolize_keys
   end
+
+  private_class_method :post
 end
